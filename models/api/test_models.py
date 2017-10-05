c131c8bda483557537d290a98c2c6176ad431e9a
Brandon Liu <liu.b97@gmail.com>
2017-10-05 09:08:12 -0400

    finish author tests

--- models/api/test_models.py
+++ models/api/test_models.py
@@ -3,39 +3,62 @@ from api.models import Author, Book, Review
 
 class AuthorTestCase(TestCase):
     def setUp(self):
-        Author.objects.create_author("J.K.", "Rowling", "52")
+        pass
 
-    def create_author(self):
-        response = self.client.post('/author/create', {'first_name': 'J.K.', 'last_name': 'Rowling', 'age': 52})
-        self.assertContains(response, 'first_name')
-        self.assertContains(response, 'last_name')
-        self.assertContains(response, 'age')
+    def test_create_author(self):
+        response = self.client.post(
+            '/api/v1/author/create', 
+            { 
+                'first_name': 'J.K.', 
+                'last_name': 'Rowling', 
+                'age': 52
+            }
+        )
+        author = Author.objects.get(first_name='J.K.')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(author.first_name, 'J.K.')
+        self.assertEqual(author.last_name, 'Rowling')
+        self.assertEqual(author.age, 52)
 
-    def read_author(self):
-        response = self.client.get('/author/1')
-        self.assertContains(response, 'first_name')
-        self.assertContains(response, 'last_name')
-        self.assertContains(response, 'age')
+    def test_read_author(self):
+        author = Author.objects.create(first_name='Stephen', last_name='King', age=50)
+        response = self.client.get('/api/v1/author/' + str(author.pk))
+        json = response.json()['result']['fields']
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json['first_name'], 'Stephen')
+        self.assertEqual(json['last_name'], 'King')
+        self.assertEqual(json['age'], 50)
 
-    def update_author(self):
-        response = self.client.post('/author/1', {'first_name': 'Jennifer', 'last_name': 'Fang', 'age': 19})
-        self.assertContains(response, 'first_name')
-        self.assertContains(response, 'last_name')
-        self.assertContains(response, 'age')
+    def test_update_author(self):
+        author = Author.objects.create(first_name='Mark', last_name='Twain', age=50)
+        response = self.client.post(
+            '/api/v1/author/' + str(author.pk), 
+            {
+                'first_name': 'Jennifer', 
+                'last_name': 'Fang', 
+                'age': 19
+            }
+        )
+        author = Author.objects.get(pk=author.pk)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(author.first_name, 'Jennifer')
+        self.assertEqual(author.last_name, 'Fang')
+        self.assertEqual(author.age, 19)
 
-    def delete_author(self):
-        self.client.get('/author/delete/1')
-        response = self.client.get('/author/1')
-        self.assertEqual(response.status_code, 404)
+    def test_delete_author(self):
+        author = Author.objects.create(first_name='Ernest', last_name='Hemmingway', age=50)
+        response = self.client.get('/api/v1/author/delete/' + str(author.pk))
+        self.assertEqual(response.status_code, 200)
+        with self.assertRaises(Author.DoesNotExist):
+            Author.objects.get(pk=author.pk)
 
-    def fails_invalid(self):
+    def test_fails_invalid(self):
         response = self.client.get('/author')
         self.assertEqual(response.status_code, 404)
 
     def tearDown(self):
         pass
-
-
+        
 
 class BookTestCase(TestCase):
     def setUp(self):
