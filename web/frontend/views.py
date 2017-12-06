497c097ad2152cce7637e6fcb519f7e0ac8c281c
Eric Xie <xiexieeric@gmail.com>
2017-12-05 22:37:54 -0500

    fixed case with no recs

--- web/frontend/views.py
+++ web/frontend/views.py
@@ -82,7 +82,7 @@ def listing_detail(request, listing_id):
 			"book_title": resp["result"]["fields"]["book_title"],
 			"price": resp["result"]["fields"]["price"],
 		}
-		if resp['recs']:
+		if 'recs' in resp:
 			context['recs'] = resp['recs']
 		req_2 = urllib.request.Request('http://exp-api:8000/api/v1/user/'+str(resp["result"]["fields"]["lister"]))
 		resp_json_2 = urllib.request.urlopen(req_2).read().decode('utf-8')
